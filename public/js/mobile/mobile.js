77c7d8eb159611bb071b33da2b8db96a939825f8
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-10-07 20:56:24 -0300

    Add libs for app

--- public/js/mobile/mobile.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.handle').on('click', function(){
-			$('nav ul').toggleClass('showing');
-	});
-	$("a").click(function(){
-        $('.showing').toggleClass();
-    });
-})
