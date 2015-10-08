a40661cb47946a7da26a58fb8f2d692db7033c26
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-10-08 15:33:12 -0300

    add express load and ejs

--- config/express.js
+++ config/express.js
@@ -1,5 +1,5 @@
 var express =  require('express')
-//	,load	= require('express-load')
+	,load	= require('express-load')
 	,bodyParser =  require('body-parser')
 	,cookieParser = require('cookie-parser')
 	,session = require('express-session')
@@ -18,14 +18,14 @@ module.exports = function(){
 		app.use(express.static('./public'));		
 		app.set('view engine','ejs');		
 		app.set('views','./app/views');		
-		// app.use(cookieParser());		
-		// app.use(session(
-		// 	{
-		// 		secret:'Aph@lbe_Met0re',
-		// 		resave:true,
-		// 		saveUnitialized:true
-		// 	}
-		// ));
+		app.use(cookieParser());		
+		app.use(session(
+			{
+				secret:'Aph@lbe_Met0re',
+				resave:true,
+				saveUnitialized:true
+			}
+		));
 
 		app.use(bodyParser.urlencoded({extended:true}));
 		app.use(bodyParser.json());
@@ -40,15 +40,15 @@ module.exports = function(){
 		app.use(helmet.hidePoweredBy({setTo:'PHP 5.5.14'}));
 
 
-		//load('models',{cwd:'app'})
-		//	.then('controllers')
-		//	.then('models')
-		//	.then('routes')
-		//	.into(app);
+		load('models',{cwd:'app'})
+			.then('controllers')
+			.then('models')
+			.then('routes')
+			.into(app);
 
-		// app.get('*', function(req,res){
-		// 	res.status(404).render('404');
-		// });
+		app.get('*', function(req,res){
+			res.status(404).render('404');
+		});
 
  return app;
 
