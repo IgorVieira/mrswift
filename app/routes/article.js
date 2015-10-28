06538000d56cb7348af9a91668236558fd1d7a2f
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-10-28 17:34:38 -0200

    create pages

--- app/routes/article.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app) {
-  var controller = app.controllers.article;
-
-  app.get('/articles', controller.listArticles);
-  app.get('/articles/:id', controller.getArticle);
-};
